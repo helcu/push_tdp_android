7003ced3cc2dba507f572ab4eeb398cc4e832a04
angelodev24 <u201316544@upc.edu.pe>
2017-10-18 18:33:53 -0500

    Modificacion de Angelo

--- app/src/main/java/pe/com/push_tdp/push_tdp/LoginActivity.java
+++ app/src/main/java/pe/com/push_tdp/push_tdp/LoginActivity.java
@@ -2,6 +2,7 @@ package pe.com.push_tdp.push_tdp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_SHORT).show();
     }
 }
