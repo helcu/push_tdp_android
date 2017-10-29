9c87adec661830afba5c278e5a168df0b1eb4574
Hugo Rosado <hugorosol17@gmail.com>
2017-10-29 15:02:35 -0500

    TabLayout and ViewPAger added.

--- app/src/main/java/pe/com/push_tdp/push_tdp/activities/MainActivity.java
+++ app/src/main/java/pe/com/push_tdp/push_tdp/activities/MainActivity.java
@@ -3,6 +3,8 @@ package pe.com.push_tdp.push_tdp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,11 +15,15 @@ import java.util.ArrayList;
 
 import pe.com.push_tdp.push_tdp.adapters.CourseAdapter;
 import pe.com.push_tdp.push_tdp.R;
+import pe.com.push_tdp.push_tdp.adapters.ViewPagerAdapter;
+import pe.com.push_tdp.push_tdp.fragments.AllPublicationsFragment;
+import pe.com.push_tdp.push_tdp.fragments.SpecificPublicationsFragment;
 import pe.com.push_tdp.push_tdp.models.Course;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView courseRecyclerView;
-    CourseAdapter courseAdapter;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +32,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        final ArrayList<Course> listCourse=new ArrayList<Course>();
-        listCourse.add(new Course(1,"Redes y Comunicacion de Datos",10, "https://www.informatica-hoy.com.ar/redes/imagenes/redes-informaticas_clip_image012.jpg"));
-        listCourse.add(new Course(2,"Estadistica Aplicada 1",20,"https://cdn.geogebra.org/material/EI3VYWGILDOAdW4LKDKopvDqBzqC9Fot/material-ieKWOaRF.png"));
-        listCourse.add(new Course(3,"Calculo 2",25, "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Spherical_Coordinates_%28Colatitude%2C_Longitude%29.svg/360px-Spherical_Coordinates_%28Colatitude%2C_Longitude%29.svg.png"));
-
-        courseRecyclerView = (RecyclerView) findViewById(R.id.courseRecyclerView);
-        courseRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        courseRecyclerView.setAdapter(new CourseAdapter(listCourse));
-
         FloatingActionButton addNewRequest = (FloatingActionButton) findViewById(R.id.addNewReqest);
+        viewPager = (ViewPager) findViewById(R.id.mainViewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new AllPublicationsFragment(), "All Publications");
+        viewPagerAdapter.addFragment(new SpecificPublicationsFragment(), "Specific Publications");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(viewPager);
+
         addNewRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
